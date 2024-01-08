b7d343dae24f187a6138380af6da60ac143d578b
Pola Sri Anvitha Gollapudi <141412212+gollapudianvitha@users.noreply.github.com>
2024-01-08 12:15:11 +0530

    myapp.py

--- myapp.py
+++ myapp.py
@@ -5,9 +5,9 @@ st.header("Types of Cartoons")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Jerry")
-  st.image("http://clipart-library.com/images/kiKByzxoT.jpg",caption="Jerry",width=300,use_column_width=True)
+  st.image("http://clipart-library.com/images/kiKByzxoT.jpg",caption="Jerry",width=200,use_column_width=True)
   st.write("Jerry is soo cute")
   with col2:
-    st.subheader("Ragdoll Cat")
-  st.image("https://2.bp.blogspot.com/-RgxTTFbpAgY/UUCAUA7CS9I/AAAAAAAAGLU/TLqTuhXjhXc/s1600/16.png",caption="Dora",width=300,use_column_width=True)
+    st.subheader("Dora")
+  st.image("https://2.bp.blogspot.com/-RgxTTFbpAgY/UUCAUA7CS9I/AAAAAAAAGLU/TLqTuhXjhXc/s1600/16.png",caption="Dora",width=200,use_column_width=True)
   st.write("Dora is sweet")
