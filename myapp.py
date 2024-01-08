814d139d2e14c1c3f81f17a304d94376cffad26e
Pola Sri Anvitha Gollapudi <141412212+gollapudianvitha@users.noreply.github.com>
2024-01-08 12:09:06 +0530

    myapp.py

--- myapp.py
+++ myapp.py
@@ -5,7 +5,7 @@ st.header("Types of Cats")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Persian Cat")
-  st.image("https://cdn.fotofits.com/petzlover/gallery/img/l/persian-552570.jpg",caption="Persian Cat",width=300,use_column_width=True)
+  st.image("https://www.thesprucepets.com/thmb/ymYiRO6J0t5H8-OiR8eeBsEIwkg=/1080x1080/filters:no_upscale():max_bytes(150000):strip_icc()/50122757_393198351489429_2336461074070557448_n-5c4cf69f46e0fb00014a2b9f.jpg",caption="Persian Cat",width=300,use_column_width=True)
   st.write("Persian cats are cute")
   with col2:
     st.subheader("Ragdoll Cat")
