d3ee716b9927bb79c8de63846dc01b80c321a583
Pola Sri Anvitha Gollapudi <141412212+gollapudianvitha@users.noreply.github.com>
2024-01-08 12:14:00 +0530

    myapp.py

--- myapp.py
+++ myapp.py
@@ -1,13 +1,13 @@
 import streamlit as st
-st.set_page_config(page_title='Cats')
-st.header("Types of Cats")
+st.set_page_config(page_title='cartoon')
+st.header("Types of Cartoons")
 
 col1,col2=st.columns(2)
 with col1:
-  st.subheader("Persian Cat")
-  st.image("https://www.thesprucepets.com/thmb/ymYiRO6J0t5H8-OiR8eeBsEIwkg=/1080x1080/filters:no_upscale():max_bytes(150000):strip_icc()/50122757_393198351489429_2336461074070557448_n-5c4cf69f46e0fb00014a2b9f.jpg",caption="Persian Cat",width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.subheader("Jerry")
+  st.image("http://clipart-library.com/images/kiKByzxoT.jpg",caption="Jerry",width=300,use_column_width=True)
+  st.write("Jerry is soo cute")
   with col2:
     st.subheader("Ragdoll Cat")
-  st.image("https://tse2.mm.bing.net/th?id=OIP.JWq61hdMTQvB-BjE_EIUcAHaHa&pid=Api&P=0&h=180",caption="Ragdoll Cat",width=300,use_column_width=True)
-  st.write("Ragdoll cats are proud")
+  st.image("https://2.bp.blogspot.com/-RgxTTFbpAgY/UUCAUA7CS9I/AAAAAAAAGLU/TLqTuhXjhXc/s1600/16.png",caption="Dora",width=300,use_column_width=True)
+  st.write("Dora is sweet")
