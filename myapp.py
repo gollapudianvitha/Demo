1dc47c284b0965a07eed74568d2b052c1bc5b4a5
Pola Sri Anvitha Gollapudi <141412212+gollapudianvitha@users.noreply.github.com>
2024-01-08 19:33:26 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title='cartoon')
 st.header("Types of Cartoons")
 
-col1,col2,col3=st.columns(3)
+col1,col2,col3=st.columns(4)
 with col1:
   st.subheader("Jerry")
   st.image("http://clipart-library.com/images/kiKByzxoT.jpg",caption="Jerry",width=200,use_column_width=True)
@@ -15,5 +15,9 @@ with col3:
   st.subheader("sinchan")
   st.image("https://media.tenor.com/7SE3IKEub60AAAAi/shinchan.gif",caption="shinchan",width=200,use_column_width=True)
   st.write("sinchan is naughty")
+with col4:
+  st.subheader("Motu and patlu")
+  st.image("https://media.tenor.com/Itzu4p4UpU0AAAAM/gale-lag-yaara-motu.gif",caption="shinchan",width=200,use_column_width=True)
+  st.write("motu and patlu both are funny")
 
 
