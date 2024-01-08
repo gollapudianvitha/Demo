e650f2ab0dc6d542633d54c9e9c04ff9dae20ca4
Pola Sri Anvitha Gollapudi <141412212+gollapudianvitha@users.noreply.github.com>
2024-01-08 12:22:17 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -7,11 +7,11 @@ with col1:
   st.subheader("Jerry")
   st.image("http://clipart-library.com/images/kiKByzxoT.jpg",caption="Jerry",width=200,use_column_width=True)
   st.write("Jerry is soo cute")
-  with col2:
-    st.subheader("Dora")
-    st.image("https://2.bp.blogspot.com/-RgxTTFbpAgY/UUCAUA7CS9I/AAAAAAAAGLU/TLqTuhXjhXc/s1600/16.png",caption="Dora",width=200,use_column_width=True)
-    st.write("Dora is sweet")
+with col2:
+  st.subheader("Dora")
+  st.image("https://2.bp.blogspot.com/-RgxTTFbpAgY/UUCAUA7CS9I/AAAAAAAAGLU/TLqTuhXjhXc/s1600/16.png",caption="Dora",width=200,use_column_width=True)
+  st.write("Dora is sweet")
  with col3:
-    st.subheader("Dora")
+    st.subheader("sinchan")
     st.image("https://media.tenor.com/7SE3IKEub60AAAAi/shinchan.gif",caption="Dora",width=200,use_column_width=True)
-    st.write("Dora is sweet")
+    st.write("sinchan is naughty")
