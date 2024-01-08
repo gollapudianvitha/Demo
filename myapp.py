97348986200bcf16c541c1276339da70a169b018
Pola Sri Anvitha Gollapudi <141412212+gollapudianvitha@users.noreply.github.com>
2024-01-08 12:23:02 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -11,7 +11,7 @@ with col2:
   st.subheader("Dora")
   st.image("https://2.bp.blogspot.com/-RgxTTFbpAgY/UUCAUA7CS9I/AAAAAAAAGLU/TLqTuhXjhXc/s1600/16.png",caption="Dora",width=200,use_column_width=True)
   st.write("Dora is sweet")
- with col3:
-    st.subheader("sinchan")
-    st.image("https://media.tenor.com/7SE3IKEub60AAAAi/shinchan.gif",caption="Dora",width=200,use_column_width=True)
-    st.write("sinchan is naughty")
+with col3:
+  st.subheader("sinchan")
+  st.image("https://media.tenor.com/7SE3IKEub60AAAAi/shinchan.gif",caption="Dora",width=200,use_column_width=True)
+  st.write("sinchan is naughty")
