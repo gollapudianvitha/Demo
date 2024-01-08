3ca96c576329ce51f89d5e6cc75f21024120ddab
Pola Sri Anvitha Gollapudi <141412212+gollapudianvitha@users.noreply.github.com>
2024-01-08 12:21:20 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title='cartoon')
 st.header("Types of Cartoons")
 
-col1,col2=st.columns(2)
+col1,col2,col3=st.columns(3)
 with col1:
   st.subheader("Jerry")
   st.image("http://clipart-library.com/images/kiKByzxoT.jpg",caption="Jerry",width=200,use_column_width=True)
@@ -10,4 +10,8 @@ with col1:
   with col2:
     st.subheader("Dora")
     st.image("https://2.bp.blogspot.com/-RgxTTFbpAgY/UUCAUA7CS9I/AAAAAAAAGLU/TLqTuhXjhXc/s1600/16.png",caption="Dora",width=200,use_column_width=True)
+    st.write("Dora is sweet")
+ with col3:
+    st.subheader("Dora")
+    st.image("https://media.tenor.com/7SE3IKEub60AAAAi/shinchan.gif",caption="Dora",width=200,use_column_width=True)
     st.write("Dora is sweet")
